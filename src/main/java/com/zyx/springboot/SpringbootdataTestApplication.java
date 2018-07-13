1cd924c14965c1dab0adf471a4353093b8ddc2b5
zyx13131133 <1195052477@qq.com>
2018-07-13 11:20:18 +0800

    first commit

---
+++ src/main/java/com/zyx/springboot/SpringbootdataTestApplication.java
@@ -0,0 +1,12 @@
+package com.zyx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdataTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdataTestApplication.class, args);
+	}
+}
