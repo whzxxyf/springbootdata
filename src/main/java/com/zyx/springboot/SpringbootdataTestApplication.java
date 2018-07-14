fafe5725f2c745e4cea7e65acc804423f8382b1b
zyx13131133 <1195052477@qq.com>
2018-07-14 18:02:39 +0800

    搞不定的MySQL配置文件

--- src/main/java/com/zyx/springboot/SpringbootdataTestApplication.java
+++ src/main/java/com/zyx/springboot/SpringbootdataTestApplication.java
@@ -2,8 +2,11 @@ package com.zyx.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//因为还没有配置数据库，他自动注入的时候就会报错
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SpringbootdataTestApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/zyx/springboot/testDataConn.java
@@ -0,0 +1,30 @@
+package com.zyx.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class testDataConn {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/springboot-data?useSSL=false";
+        String driver = "com.mysql.jdbc.Driver";
+        try{
+            Class clazz = Class.forName(driver);
+        }catch(Exception e){
+            System.out.println("无法加载驱动");
+        }
+
+        try {
+            Connection con = DriverManager.getConnection(url,"root","123456");
+            if(!con.isClosed())
+                System.out.println("success");
+                System.out.println(con);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
