34c66fca31909e36196174b5e908f08f8f4b22fe
zyx13131133 <1195052477@qq.com>
2018-07-16 09:24:55 +0800

    可以连接mysql，配置文件中密码是password，开始写错了

--- src/test/java/com/zyx/springboot/SpringbootdataTestApplicationTests.java
+++ src/test/java/com/zyx/springboot/SpringbootdataTestApplicationTests.java
@@ -2,15 +2,25 @@ package com.zyx.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootdataTestApplicationTests {
 
+	@Autowired
+	DataSource dataSource;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
+		System.out.println(dataSource.getClass());
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
 	}
 
 }
